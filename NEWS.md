a02a22c062558d18d7c1020ce4d2a44741c6992e
Christoph Sax <christoph.sax@gmail.com>
2021-03-22 14:30:06 +0000

    version 0.1.1

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+# indiedown 0.1.1
+
+- CRAN feedback: better DESCRIPTION, extended documentation, examples.
+
+# indiedown 0.1.0
+
+Initial release.
+
+- `create_indiedown_package()` creates a ready-to-customize R Markdown template package.
+- `use_indiedown_gfonts()` adds Google fonts to a template package.
---
+++ inst/doc/indiedown.R
@@ -0,0 +1,23 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----eval = FALSE-------------------------------------------------------------
+#  install.packages("indiedown")
+
+## ----eval = FALSE-------------------------------------------------------------
+#  # install.packages("remotes")
+#  remotes::install_github("cynkra/indiedown")
+
+## ----eval = FALSE-------------------------------------------------------------
+#  # install.packages("devtools")
+#  devtools::install_github("cynkra/indiedown")
+
+## ----eval = FALSE-------------------------------------------------------------
+#  indiedown::create_indiedown_package("mydown")
+
+## ----eval = FALSE-------------------------------------------------------------
+#  devtools::install("mydown")
+
